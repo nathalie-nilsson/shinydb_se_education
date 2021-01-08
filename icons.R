47cf37391e6adf9863b2ab5080323d91ce90f873
nathalie-nilsson <nath.nils@gmail.com>
2021-01-08 14:47:20 +0100

    Create 'icon.R' file; customized icons

---
+++ icons.R
@@ -0,0 +1,27 @@
+# ==============================================================================
+#   
+#  Script name ...... icons.R
+#  Project .......... shinydb_se_education
+#  Script purpose ... Create custom formatted icons
+#
+#  Author ........... Nathalie Nilsson
+#
+#  Date created ..... 2021-01-08
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+my.play <- HTML('<i class="glyphicon glyphicon-play-circle" style="
+    color: orange;
+    top: 20px;
+    right: 50%;
+    font-size: x-large;
+    /* font-weight: 900; */
+"></i>')
+
+my.pause <- HTML('<i class="glyphicon glyphicon-pause" style="
+    color: orange;
+    top: 20px;
+    right: 50%;
+    font-size: x-large;
+    /* font-weight: 900; */
+"></i>')
